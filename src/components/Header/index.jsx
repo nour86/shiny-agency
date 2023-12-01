70dc47057f457f9540f2a57a4438ade1dcdd54f8
Nour Alchami <n.alchami@gmail.com>
2023-12-01 13:40:05 +0100

    css in js

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,25 @@
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
+const StyledLink = styled(Link)`
+  padding: 15px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 18px;
+  ${(props) =>
+    props.$isFullLink &&
+    `color: white; border-radius: 30px; background-color: #5843E4;`}
+`
 function Header() {
   return (
     <nav>
-      <Link to="/">Accueil</Link>
-      <Link to="/survey">Questionnaire</Link>
+      <StyledLink to="/" $isFullLink>
+        Accueil
+      </StyledLink>
+      <StyledLink to="/survey/1" $isFullLink>
+        Questionnaire
+      </StyledLink>
+      <StyledLink to="/freelances">Profils</StyledLink>
     </nav>
   )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header/'
-import Home from './pages/Home/'
-import Survey from './pages/Survey/'
-import ClientForm from './components/ClientForm'
-import FreelanceForm from './components/FreelanceForm'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Survey from './pages/Survey'
+import Results from './pages/Results'
+import Freelances from './pages/Freelances'
+import Header from './components/Header'
+import Error from './components/Error'
+import { createGlobalStyle } from 'styled-components'
 
+const GlobalStyle = createGlobalStyle`
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+`
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <GlobalStyle />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/survey" element={<Survey />}>
-          <Route path="client" element={<ClientForm />} />
-          <Route path="freelance" element={<FreelanceForm />} />
-        </Route>
+        <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/freelances" element={<Freelances />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   </React.StrictMode>,
